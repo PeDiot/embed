301bba8bcb809ea7448b5c080c0007410f7a67c8
PeDiot <p.emmanuel.diot@gmail.com>
2025-01-09 23:45:03 +0100

    clean

--- src/enums.py
+++ src/enums.py
@@ -1,12 +1,10 @@
 PROJECT_ID = "carlia"
 DATASET_ID = "vinted"
+
 CATALOG_TABLE_ID = "catalog"
 ITEM_TABLE_ID = "item"
 IMAGE_TABLE_ID = "image"
 PINECONE_TABLE_ID = "pinecone"
 CATEGORY_TABLE_ID = "category"
 
-QDRANT_COLLECTION_NAME = "items"
 PINECONE_INDEX_NAME = "items"
-QDRANT_PAYLOAD_FIELDS = ["women", "category_type", "price"]
-EMBED_DIM = 512
